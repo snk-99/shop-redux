e32d584eb2d27f9b59f9301f70e3304a85b4cdd2
samuel kahura <kahurasamuel@gmail.com>
2021-10-02 18:08:36 -0400

    client/pages done

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/react-hooks';
+
+import { QUERY_PRODUCTS } from "../utils/queries";
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+
+// import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
 } from '../utils/actions';
-import { QUERY_PRODUCTS } from '../utils/queries';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state;
+  });
   const { id } = useParams();
+  const dispatch = useDispatch();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
